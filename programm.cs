e7f3d57303ed3c84deb7c452170ac99ead9b097a
HooBaWork <121292182+HooBaWork@users.noreply.github.com>
2023-03-15 12:14:58 +0300

    Update programm.cs
    Добавили метод,формирующий из нашего массива массив,с ограничением длины элементов массива .

--- programm.cs
+++ programm.cs
@@ -3,6 +3,20 @@ Console.WriteLine("Укажите размер массива");
 int size = int.Parse(Console.ReadLine());
 string[] resultArray = returnArray();
 
+string[] finalArray(string[] resultArray)
+{
+    string[] fArray = new string[size];
+    for(int i = 0; i < size;i++)
+    {
+        if(resultArray[i].Length <= 3 )
+        {
+            fArray[i] = resultArray[i];
+           Console.Write($"{fArray[i]}\t ");
+        }
+    }
+    return fArray;
+}
+
 string[] returnArray()
 {
    string[] array = new string[size];
