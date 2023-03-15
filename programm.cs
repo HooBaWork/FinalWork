949d21e5eb047388e0cdbec71f6709c218738c6e
HooBaWork <121292182+HooBaWork@users.noreply.github.com>
2023-03-15 12:33:23 +0300

    Update programm.cs
    Отформатировали код ,добавили вызов функции для получения необходимого массива .

--- programm.cs
+++ programm.cs
@@ -1,29 +1,35 @@
 Console.Clear();
-Console.WriteLine("Укажите размер массива");
-int size = int.Parse(Console.ReadLine());
-string[] resultArray = returnArray();
+Console.WriteLine("Введите размер массива");
+     int size = int.Parse(Console.ReadLine());
+
+void main()
+{
+    string[] resultArray = returnArray();
+    string[] finalResult = finalArray(resultArray);
+}
+main();
+
+string[] returnArray()
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите элемент {i + 1} массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
 
 string[] finalArray(string[] resultArray)
 {
     string[] fArray = new string[size];
-    for(int i = 0; i < size;i++)
+    for (int i = 0; i < size; i++)
     {
-        if(resultArray[i].Length <= 3 )
+        if (resultArray[i].Length <= 3)
         {
             fArray[i] = resultArray[i];
-           Console.Write($"{fArray[i]}\t ");
+            Console.Write($"{fArray[i]}\t ");
         }
     }
     return fArray;
-}
-
-string[] returnArray()
-{
-   string[] array = new string[size];
-   for(int i = 0;i < size;i++)
-   {
-    Console.WriteLine($"Введите элемент {i+1} массива");
-    array[i] = Console.ReadLine();
-   }
-   return array;
 }
